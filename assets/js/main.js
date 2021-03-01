f5d0fc0feccc6e086feaf5bc8ff4e4d6b5764406
gabriele <sozziga@gmail.com>
2021-03-01 17:12:04 +0100

    Completato.
    Creata struttura js ed esercizio completato. Aggiungo  bonus con css per renderlo esteticamente migliore.

--- assets/js/main.js
+++ assets/js/main.js
@@ -13,12 +13,25 @@ console.log(anni);
 
 // Calcolo prezzo e scontistica da applicare in relazione all'età
 
-var prezzo = console.log(km * 0.21 + "€");
+var prezzo = km * 0.21;
 
 if (anni > 65) {
-  var prezzo = console.log(prezzo - (prezzo * 0.4));
+  var over65 = prezzo - (prezzo * 0.4);
+  console.log(over65);
 } else if (anni < 18) {
-  
+  var under18 = prezzo - (prezzo * 0.2);
+  console.log(under18);
 } else {
-  var prezzo = console.log(km * 0.21 + "€");
+  var prezzo = prezzo;
+  console.log(prezzo);
+}
+
+// Mostra il prezzo nel DOM in elemento con id = amount
+
+if (anni > 65) {
+  document.getElementById('amount').innerHTML = "Il totale del tuo biglietto è " + over65 + "€";
+} else if (anni < 18) {
+  document.getElementById('amount').innerHTML = "Il totale del tuo biglietto è " + under18 + "€";
+} else {
+  document.getElementById('amount').innerHTML = "Il totale del tuo biglietto è " + prezzo + "€";
 }
